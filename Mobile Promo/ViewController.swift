88d928f1aea927c2ea9e902b2a241bcbde5f2698
William-Yulio <william.yulio331@gmail.com>
2023-08-04 18:43:50 +0700

    Initial Commit

---
+++ Mobile Promo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mobile Promo
+//
+//  Created by William Yulio on 04/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
